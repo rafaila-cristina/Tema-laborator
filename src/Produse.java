715b562ab8c4376736038f8844dd2b3fb6be6d39
rafaila-cristina <rafaila.cristina@gmail.com>
2020-11-16 11:57:57 +0200

    Adaugare metode get si set
    Am adaugate metodele get si set pentru toti membrii
    
    Signed-off-by: rafaila-cristina <rafaila.cristina@gmail.com>

--- src/Produse.java
+++ src/Produse.java
@@ -4,6 +4,30 @@ public class Produse {
     private int pret;
     private String cod;
 
+    public String getDenumire() {
+        return denumire;
+    }
+
+    public int getPret() {
+        return pret;
+    }
+
+    public String getCod() {
+        return cod;
+    }
+
+    public void setDenumire(String denumire) {
+        this.denumire = denumire;
+    }
+
+    public void setPret(int pret) {
+        this.pret = pret;
+    }
+
+    public void setCod(String cod) {
+        this.cod = cod;
+    }
+
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
