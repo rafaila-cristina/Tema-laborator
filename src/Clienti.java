8afc51e242e919da26e5c36a2809edd6a009bf3c
patra <alin.patrascu@mta.ro>
2020-11-16 11:47:40 +0200

    Adugare functii
    Am adaugat functie de datorie a clientului si functii de set a numelui, tipului si datorii.
    
    Signed-off-by: patra <alin.patrascu@mta.ro>

--- src/Clienti.java
+++ src/Clienti.java
@@ -1,4 +1,47 @@
+import java.util.ArrayList;
+
 public class Clienti {
-    
+        private ArrayList<String> listaClienti;
+        private String tipClient;
+        private String nume;
+        private int datorii;
+
+        void set_tip(String tip)
+        {
+            this.tipClient=tip;
+        }
+
+        void set_nume(String nume)
+        {
+            this.nume=nume;
+        }
+
+        void set_datorii(int n)
+        {
+            this.datorii=n;
+        }
+
+        void adugare_datorii(int n)
+        {
+            this.datorii+=n;
+        }
+
+        void platire_datorii(int n)
+        {
+            this.datorii=this.datorii-n;
+        }
+
+        
+
+    @Override
+    public String toString() {
+        return "Clienti{" +
+                "listaClienti=" + listaClienti +
+                ", tipClient='" + tipClient + '\'' +
+                ", nume='" + nume + '\'' +
+                ", datorii=" + datorii +
+                '}';
+    }
+
 
 }
