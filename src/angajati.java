971765ee01124aeea223a7227c9bff49dbf3fc76
pope <popescubogdanalx@yahoo.com~>
2020-11-16 11:32:09 +0200

    Adaugare metode Angajati
    AM adaugat cateva metode la clasa Angajati.
    
    Signed-off-by: pope <popescubogdanalx@yahoo.com~>

--- src/angajati.java
+++ src/angajati.java
@@ -1,2 +1,30 @@
+import java.io.*;
+import java.util.*;
+
 public class angajati {
+    private ArrayList<String> mAngajati;
+
+    public angajati(ArrayList<String> mAngajati) {
+        this.mAngajati = mAngajati;
+    }
+
+    public ArrayList<String> getmAngajati() {
+        return mAngajati;
+    }
+
+    public void setmAngajati(ArrayList<String> mAngajati) {
+        this.mAngajati = mAngajati;
+    }
+
+    @Override
+    public String toString() {
+        return "angajati{" +
+                "mAngajati=" + mAngajati +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
 }
